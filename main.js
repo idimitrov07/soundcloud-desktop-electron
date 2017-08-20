f39b1247fc9fa5d3156cb683455ebe5166029927
Ivan Dimitrov <idimitrov07@gmail.com>
2017-08-20 20:25:14 +0300

    Load website in background before showing the app.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 960, height: 600})
+  win = new BrowserWindow({width: 960, height: 600, show: false})
 
   // and load the index.html of the app.
   // win.loadURL(url.format({
@@ -20,8 +20,12 @@ function createWindow () {
   // open soundcloud
   win.loadURL('https://soundcloud.com')
 
+  win.once('ready-to-show', () => {
+    win.show()
+  })
+
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
